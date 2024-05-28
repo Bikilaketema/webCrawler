814c07efe7fec272bcdea5ac397ab26595ffd720
Bikila Ketema <82276576+Bikilaketema@users.noreply.github.com>
2024-05-28 08:18:46 +0000

    minor improvements

--- browsing_app_by_ui/src/components/DocumentList.js
+++ browsing_app_by_ui/src/components/DocumentList.js
@@ -2,14 +2,14 @@ import React from 'react';
 
 const DocumentList = ({ documents }) => {
     return (
-        <ul class="m-10">
+        <ol class="m-10 list-decimal text-white">
             {documents.map((document, index) => (
                 <li key={index} class="bg-blue-400 rounded-md m-4 p-2">
                     <h3 class="text-white font-bold text-xl">{document.title}</h3>
                     <p>{document.summary}</p>
                 </li>
             ))}
-        </ul>
+        </ol>
     );
 };
 
